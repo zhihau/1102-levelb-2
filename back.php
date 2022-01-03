fbcffa9a58fd238b371265bc3009f900565c7454
zhihau <shiusoft@gmail.com>
2022-01-03 16:12:31 +0800

    後台頁面建置

--- back.php
+++ back.php
@@ -21,11 +21,11 @@
 		<?php include "front/header.php" ?>
 		<div id="mm">
 			<div class="hal" id="lef">
+				<a class="blo" href="?do=admin">帳號管理</a>
 				<a class="blo" href="?do=po">分類網誌</a>
-				<a class="blo" href="?do=news">最新文章</a>
-				<a class="blo" href="?do=pop">人氣文章</a>
-				<a class="blo" href="?do=know">講座訊息</a>
-				<a class="blo" href="?do=que">問卷調查</a>
+				<a class="blo" href="?do=news">最新文章管理</a>
+				<a class="blo" href="?do=know">講座管理</a>
+				<a class="blo" href="?do=que">問卷管理</a>
 			</div>
 			<div class="hal" id="main">
 				<div>
@@ -34,7 +34,7 @@
 						if (isset($_SESSION['login'])) {
 							if ($_SESSION['login'] == 'admin') {
 						?>
-								歡迎admin，<br><button onclick="location.href='back.php'">管理</button>|<button onclick="logout()">登出</button>
+								歡迎admin，<br><button>管理</button>|<button onclick="logout()">登出</button>
 							<?php
 							} else {
 							?>
@@ -57,11 +57,11 @@
 					<div class="">
 						<?php
 						$do = $_GET['do'] ?? 'home';
-						$file = 'front/' . $do . ".php";
+						$file = 'back/' . $do . ".php";
 						if (file_exists($file)) {
 							include $file;
 						} else {
-							include 'front/home.php';
+							include 'back/home.php';
 						}
 						?>
 					</div>
