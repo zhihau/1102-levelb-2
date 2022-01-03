521e329c15ccd184dc43a72dca975820562ae42c
zhihau <shiusoft@gmail.com>
2022-01-03 13:10:34 +0800

    登入頁

---
+++ front/forget.php
@@ -0,0 +1,7 @@
+<fieldset>
+    <legend>忘記密碼</legend>
+    <div>請輸入信箱以查詢密碼</div>
+    <div><input type="text" name="email" id="email"></div>
+    <div></div>
+    <div><button>尋找</button></div>
+</fieldset>
---
+++ front/login.php
@@ -0,0 +1,24 @@
+<fieldset>
+<legend>會員登入</legend>
+<table>
+    <tr>
+        <td>帳號</td>
+        <td><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td>密碼</td>
+        <td><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td>
+            <button>登入</button>
+            <button>清除</button>
+        </td>
+        <td>
+            <a href="index.php?do=forget">忘記密碼</a>
+            <a href="index.php?do=reg">尚未註冊</a>
+        </td>
+    </tr>
+</table>
+
+</fieldset>
